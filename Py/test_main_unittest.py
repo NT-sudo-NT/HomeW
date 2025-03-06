d311396d209ddaf4008ab472894d2928311326eb
NT-sudo-NT <201159248+NT-sudo-NT@users.noreply.github.com>
2025-03-06 13:11:03 +0300

    Update test_main_unittest.py

--- Py/test_main_unittest.py
+++ Py/test_main_unittest.py
@@ -1,30 +1,6 @@
+from main import rev, is_palindrome, count_vowels, find_repeated_chars
+
 import unittest
-
-def rev(s):
-    """Возвращает строку в обратном порядке."""
-    return s[::-1]
-
-def is_palindrome(s):
-    """Проверяет, является ли строка палиндромом."""
-    s = ''.join(s.lower().split())
-    return s == rev(s)
-
-def count_vowels(s):
-    """Считает количество гласных букв в строке."""
-    return sum(1 for char in s.lower() if char in 'aeiou')
-
-def find_repeated_chars(s):
-    # Находит повторяющиеся символы в строке.
-    s = s.lower()
-    char_count = {}
-    # Используем один проход по строке для подсчёта символов, вместо многократного вызова count.
-    for char in s:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    # Возвращаем только символы, которые встречаются более одного раза.
-    return {char: count for char, count in char_count.items() if count > 1}
 
 class TestingStringFunctions(unittest.TestCase):
     
